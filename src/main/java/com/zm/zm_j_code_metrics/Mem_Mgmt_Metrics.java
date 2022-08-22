f65f98e9091ad78fa686a59c17b4f93e2960dc8e
Mohammed ZAGANE <m_zagane@yahoo.fr>
2022-08-22 15:04:21 +0100

    Class creation

---
+++ src/main/java/com/zm/zm_j_code_metrics/Mem_Mgmt_Metrics.java
@@ -0,0 +1,19 @@
+/**
+ * ZM J Code Metrics 2
+ * 
+ * file : code related to Taint metrics
+ * src date: 22.08.2022
+ * src version: 22.08.2022
+ * 
+ * @author ZM (ZAGANE Mohammed)
+ * @email : m_zagane@yahoo.fr
+ */
+package com.zm.zm_j_code_metrics;
+
+/**
+ *
+ * @author zm
+ */
+public class Mem_Mgmt_Metrics {
+    
+}
